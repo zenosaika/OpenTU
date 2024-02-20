a73094f07e145e12653f944980984a7b1f5bff87
zenosaika <zenosaika@gmail.com>
2024-02-21 00:15:25 +0700

    roommate list

--- OpenTU/Dorm/views.py
+++ OpenTU/Dorm/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from User.models import Student
 from .models import Room, Bill
 
+@login_required
 def dorm_request(request):
-    room = Room.objects.filter()
-    return render(request, 'Dorm/dorm.html', {})
+    student = Student.objects.filter(user=request.user)
+    context = {}
+    if student:
+        room_id = student[0].room.pk
+        if room_id:
+            room = Room.objects.filter(pk=room_id)
+            bills = Bill.objects.filter(room_id=room_id)
+
+            context['roommates'] = Student.objects.filter(room=room_id)
+            context['electric_cost'] = sum(bill.electric_cost for bill in bills if bill.payment_status==False)
+            context['water_cost'] = sum(bill.water_cost for bill in bills if bill.payment_status==False)
+
+    return render(request, 'Dorm/dorm.html', context)
