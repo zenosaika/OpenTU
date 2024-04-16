f413883321e44e1c33754647efbcb40b0ab92864
zenosaika <zenosaika@gmail.com>
2024-04-17 03:48:59 +0700

    transport ev bus

--- OpenTU/Transport/models.py
+++ OpenTU/Transport/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Bus(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+    
+class Station(models.Model):
+    name = models.CharField(max_length=60)
+    type = models.CharField(max_length=60)
+    available_buses = models.ManyToManyField(Bus, blank=True)
+
+    def __str__(self):
+        return self.name
--- OpenTU/Transport/views.py
+++ OpenTU/Transport/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from .models import Station, Bus
+from datetime import datetime
 
 def transport_request(request):
     context = {}
+
+    stations = Station.objects.all()
+    red_stations = []
+    yellow_stations = []
+    green_stations = []
+    purple_stations = []
+
+    for station in stations:
+        station.available_buses_list = [b for b in station.available_buses.all()]
+        if station.type == 'red':
+            red_stations.append(station)
+        elif station.type == 'yellow':
+            yellow_stations.append(station)
+        elif station.type == 'green':
+            green_stations.append(station)
+        elif station.type == 'purple':
+            purple_stations.append(station)
+
+    context['red_stations'] = red_stations
+    context['yellow_stations'] = yellow_stations
+    context['green_stations'] = green_stations
+    context['purple_stations'] = purple_stations
+
+    context['time'] = datetime.now().strftime("%H:%M:%S")
+
     return render(request, 'Transport/transport.html', context)
